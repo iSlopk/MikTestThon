f825344623d9d5b4977e40d7091ca3bb6632c027
iSlopk <110035853+iSlopk@users.noreply.github.com>
2025-07-08 07:22:59 +0300

    update Tepthon/plugins/team_points.py

--- Tepthon/plugins/team_points.py
+++ Tepthon/plugins/team_points.py
@@ -484,7 +484,7 @@ async def show_top_in_teams(event):
         text += f"\n• Top 3 :\n"
 
         if not top_members:
-            text += "ㅤ- ( لا يوجد أعضاء )\n"
+            text += "\nㅤ- ( لا يوجد أعضاء )\n"
         else:
             for i, (uid, pts) in enumerate(top_members[:3], start=1):
                 user = await event.client.get_entity(uid)
