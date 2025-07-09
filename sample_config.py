ac00fc58496e9ebe8ed5ae9e409d5227d68b5ad2
iSlopk <110035853+iSlopk@users.noreply.github.com>
2025-07-09 20:45:48 +0300

    update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -31,7 +31,7 @@ class Config(object):
     # Custom bot tokens
     MLIST_BOT_TOKEN = os.environ.get("MLIST_BOT_TOKEN", None)
     POINTS_BOT_TOKEN = os.environ.get("POINTS_BOT_TOKEN", None)
-    AI_BOT_TOKEN = os.environ.get("AI_BOT_TOKEN", None)
+    """AI_BOT_TOKEN = os.environ.get("AI_BOT_TOKEN", None)"""
     # get this value from http://www.timezoneconverter.com/cgi-bin/findzone.tzc
     TZ = os.environ.get("TZ", "Asia/Riyadh")
     # set this with required zed repo link
