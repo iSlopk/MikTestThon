0d9f1af3f0ec21caa141d239340fe7437124be02
iSlopk <110035853+iSlopk@users.noreply.github.com>
2025-07-20 21:28:36 +0300

    update Tepthon/plugins/status.py

--- Tepthon/plugins/status.py
+++ Tepthon/plugins/status.py
@@ -2,9 +2,9 @@ import os
 import urllib
 
 from telethon.tl import functions
-from Tepthon import zedub
-from Tepthon.core.managers import edit_delete, edit_or_reply
-from Tepthon.sql_helper.globals import addgvar, gvarstatus
+from Mikthon import zedub
+from Mikthon.core.managers import edit_delete, edit_or_reply
+from Mikthon.sql_helper.globals import addgvar, gvarstatus
 
 plugin_category = "utils"
 
