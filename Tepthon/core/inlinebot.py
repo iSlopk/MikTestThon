17b9fa2a201fb2e8609bbf2a1f7046214960464a
iSlopk <110035853+iSlopk@users.noreply.github.com>
2025-07-20 21:27:31 +0300

    update Tepthon/core/inlinebot.py

--- Tepthon/core/inlinebot.py
+++ Tepthon/core/inlinebot.py
@@ -12,7 +12,7 @@ from telethon.errors import QueryIdInvalidError
 from telethon.events import CallbackQuery, InlineQuery
 from youtubesearchpython import VideosSearch
 
-from Tepthon import zedub
+from Mikthon import zedub
 
 from ..Config import Config
 from ..helpers.functions import rand_key
