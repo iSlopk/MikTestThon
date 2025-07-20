97701ed94c4354278eacd28d1bb2c9a8443b984c
iSlopk <110035853+iSlopk@users.noreply.github.com>
2025-07-20 21:40:42 +0300

    update Tepthon/helpers/functions/functions.py

--- Tepthon/helpers/functions/functions.py
+++ Tepthon/helpers/functions/functions.py
@@ -98,8 +98,8 @@ async def covidindia(state):
 async def post_to_telegraph(
     page_title,
     html_format_content,
-    auth_name="Tepthon",
-    auth_url="https://t.me/Tepthon",
+    auth_name="Mikthon",
+    auth_url="https://t.me/Slopk",
 ):
     post_client = TelegraphPoster(use_api=True)
     post_client.create_api_token(auth_name)
