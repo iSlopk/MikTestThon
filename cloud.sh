f1ca34d583ee87e68a6e9f2edbe7ee97de23ce21
iSlopk <110035853+iSlopk@users.noreply.github.com>
2025-07-24 10:00:42 +0300

    update cloud.sh

--- cloud.sh
+++ cloud.sh
@@ -2,16 +2,16 @@
 
 cat <<'EOF'
 ---------------------------------------
-Installing Tepthon is in progress..                                                  
+Installing Mikthon is in progress..                                                  
 ---------------------------------------                                                  
 
 
                                                   
-Copyright (C) 2020-2024 by TepthonArabic@Github, < https://github.com/zxaax >.
-This file is part of < https://github.com/zxaax/TepA > project,
+Copyright (C) 2020-2025 by iSlopk@Github, < https://github.com/iSlopk >.
+This file is part of < https://github.com/iSlopk/Mikthon > project,
 and is released under the "GNU v3.0 License Agreement".
-Please see < https://github.com/zxaax/TepA/blob/main/LICENSE >
+Please see < https://github.com/iSlopk/Mikthon/blob/main/LICENSE >
 All rights reserved.
 EOF
 
-gunicorn app:app --daemon && python -m Tepthon
+gunicorn app:app --daemon && python -m Mikthon
