9051afae681b51a5896d627a2d4d35f3193d85b6
iSlopk <110035853+iSlopk@users.noreply.github.com>
2025-07-24 10:05:13 +0300

    update Tepthon/core/pluginManager.py

--- Tepthon/core/pluginManager.py
+++ Tepthon/core/pluginManager.py
@@ -76,7 +76,7 @@ async def restart_script(client: TelegramClient, sandy):
     except Exception as e:
         LOGS.error(e)
     executable = sys.executable.replace(" ", "\\ ")
-    args = [executable, "-m", "Tepthon"]
+    args = [executable, "-m", "Mikthon"]
     os.execle(executable, *args, os.environ)
     os._exit(143)
 
